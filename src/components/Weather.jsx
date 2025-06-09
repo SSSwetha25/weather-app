a2cbc681e78bad3bb811392de3634c71bcc92d7d
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-06-09 23:51:52 +0530

    Initial commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Weather.css'
+import search from '../assets/search.png'
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className='search-bar'>
+            <input type='text' placeholder='Search'/>
+            <img src={search} width={30} alt=""/>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Weather
